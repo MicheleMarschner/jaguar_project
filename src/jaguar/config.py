de2000fab4006e752d15fc50bd5636a53cdd167b
Michele Marschner <michele.marschner@gmail.com>
2026-02-21 11:57:58 +0100

    tmp for synch

--- src/jaguar/config.py
+++ src/jaguar/config.py
@@ -1,4 +1,32 @@
+from pathlib import Path
 import torch
+import os
+from dataclasses import dataclass
+import nltk
+
+@dataclass(frozen=True)
+class Paths:
+    """Holds the main filesystem paths used by the project"""
+    data: Path
+    results: Path
+    runs: Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]  # repo root (…/BeyondLiteral_Idiomaticity_Detection)
+
+IN_COLAB = True
+
+if IN_COLAB:
+    PATHS = Paths(
+        data=Path("/data"),
+        results=Path("/results"),
+        exp=Path("/experiments"),
+    )
+else:
+    PATHS = Paths(
+        data=PROJECT_ROOT / "data",
+        results=PROJECT_ROOT / "results",
+        exp=PROJECT_ROOT / "experiments",
+    )
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 NUM_WORKERS = 0     # easier to ensure reproducibility

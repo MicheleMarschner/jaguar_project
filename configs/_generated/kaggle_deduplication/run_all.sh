6d5bc93284f75336c357493028ecd3c2d62c88fb
Michele Marschner <michele.marschner@gmail.com>
2026-03-09 15:54:07 +0100

    baseline set - training start

--- configs/_generated/kaggle_deduplication/run_all.sh
+++ configs/_generated/kaggle_deduplication/run_all.sh
@@ -13,6 +13,6 @@ python src/jaguar/main.py --base_config base/kaggle_base --experiment_config _ge
 python src/jaguar/experiments/experiment_setup.py --setup_name kaggle_deduplication --base_config base/kaggle_base --experiment_config _generated/kaggle_deduplication/closed_curated_traink_3_valk_3_p4
 python src/jaguar/main.py --base_config base/kaggle_base --experiment_config _generated/kaggle_deduplication/closed_curated_traink_3_valk_3_p4 --experiment_name closed_curated_traink_3_valk_3_p4
 
-python src/jaguar/experiments/experiment_setup.py --setup_name kaggle_deduplication --base_config base/kaggle_base --experiment_config _generated/kaggle_deduplication/closed_curated_traink_3_valk_3_p4
-python src/jaguar/main.py --base_config base/kaggle_base --experiment_config _generated/kaggle_deduplication/closed_curated_traink_3_valk_3_p4 --experiment_name closed_curated_traink_3_valk_3_p4
+python src/jaguar/experiments/experiment_setup.py --setup_name kaggle_deduplication --base_config base/kaggle_base --experiment_config _generated/kaggle_deduplication/closed_curated_traink_1_valk_1_p4
+python src/jaguar/main.py --base_config base/kaggle_base --experiment_config _generated/kaggle_deduplication/closed_curated_traink_1_valk_1_p4 --experiment_name closed_curated_traink_1_valk_1_p4
 

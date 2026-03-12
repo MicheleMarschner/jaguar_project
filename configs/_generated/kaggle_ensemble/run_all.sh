aed08729ca4129f522ea224ca9fb9d5bd2605424
Michele Marschner <michele.marschner@gmail.com>
2026-03-12 18:45:16 +0100

    changed folder structure and created slurm script for xai_metrics

--- configs/_generated/kaggle_ensemble/run_all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-python src/jaguar/run_ensemble.py --base_config base/ensemble_base --experiment_config _generated/kaggle_ensemble/eva04_miewid04_conv02 --experiment_name eva04_miewid04_conv02
-
